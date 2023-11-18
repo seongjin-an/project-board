5d7ee797a0d70697add002ffbde3fba768d02a95
seongjin-an <seongjin4us@gmail.com>
2023-11-18 18:40:06 +0900

    #71 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/main/java/com/ansj/demo/config/SecurityConfig.java
+++ src/main/java/com/ansj/demo/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.ansj.demo.config;
 
 import com.ansj.demo.dto.UserAccountDto;
 import com.ansj.demo.dto.security.BoardPrincipal;
+import com.ansj.demo.dto.security.KakaoOAuth2Response;
 import com.ansj.demo.repository.UserAccountRepository;
+import com.ansj.demo.service.UserAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +15,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -31,7 +39,25 @@ public class SecurityConfig {
      * 허용된 path는 /articles, /articles/search-hashtag
      */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+    ) throws Exception {
+//        return http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+//                        .mvcMatchers(
+//                                HttpMethod.GET,
+//                                "/",
+//                                "/articles",
+//                                "/articles/search-hashtag"
+//                        ).permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin().and()
+//                .logout()
+//                .logoutSuccessUrl("/").and()
+//                .build();
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
@@ -43,9 +69,13 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin().and()
-                .logout()
-                .logoutSuccessUrl("/").and()
+                .formLogin(withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(oauth -> oauth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService)
+                        )
+                )
                 .build();
     }
 
@@ -61,13 +91,53 @@ public class SecurityConfig {
     /**
      * 실제 인증 데이터를 가져오는 서비스 로직을 구현
      */
+//    @Bean
+//    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+//        return username -> userAccountRepository.findById(username)
+//                .map(UserAccountDto::from)
+//                .map(BoardPrincipal::from)
+//                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username:" + username));
+//    }
     @Bean
-    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
-        return username -> userAccountRepository.findById(username)
-                .map(UserAccountDto::from)
+    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
+        return username -> userAccountService
+                .searchUser(username)
                 .map(BoardPrincipal::from)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username:" + username));
     }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            UserAccountService userAccountService,
+            PasswordEncoder passwordEncoder
+    ) {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            // loadUser 는 OAuth user service 인터페이스를 구현하고자할 때 구현했어야 했던 함수명이다.
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            KakaoOAuth2Response kakaoResponse = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+            String registrationId = userRequest.getClientRegistration().getRegistrationId(); // "kakao"
+            String providerId = String.valueOf(kakaoResponse.id());
+            String username = registrationId + "_"+ providerId;
+            String dummyPassword = passwordEncoder.encode("{bcrypt}dummy");
+            return userAccountService.searchUser(username)
+                    .map(BoardPrincipal::from)
+                    .orElseGet(() ->
+                            BoardPrincipal.from(
+                                    userAccountService.saveUser(
+                                            username,
+                                            dummyPassword,
+                                            kakaoResponse.email(),
+                                            kakaoResponse.nickname(),
+                                            null
+                                    )
+                            )
+                    );
+        };
+    }
+
 
     /**
      * 패스워드 인코더
--- src/test/java/com/ansj/demo/config/TestSecurityConfig.java
+++ src/test/java/com/ansj/demo/config/TestSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.ansj.demo.config;
 
 import com.ansj.demo.domain.UserAccount;
+import com.ansj.demo.dto.UserAccountDto;
 import com.ansj.demo.repository.UserAccountRepository;
+import com.ansj.demo.service.UserAccountService;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -9,16 +12,30 @@ import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.*;
+
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 
-    @MockBean private UserAccountRepository userAccountRepository;
+//    @MockBean private UserAccountRepository userAccountRepository;
+
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetUp() {
-        BDDMockito.given(userAccountRepository.findById(BDDMockito.anyString()))
-                .willReturn(Optional.of(UserAccount.of(
-                        "unoTest", "pw", "ansj@test-mail.com", "ansj-test", "test memo"
-        )));
+//        BDDMockito.given(userAccountRepository.findById(BDDMockito.anyString()))
+//                .willReturn(Optional.of(UserAccount.of(
+//                        "unoTest", "pw", "ansj@test-mail.com", "ansj-test", "test memo"
+//        )));
+        BDDMockito.given(userAccountService.searchUser(BDDMockito.anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        BDDMockito.given(userAccountService.saveUser(anyString(), anyString(),anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                "unoTest", "pw", "ansj@test-mail.com", "ansj-test", "test memo"
+        );
     }
 }
