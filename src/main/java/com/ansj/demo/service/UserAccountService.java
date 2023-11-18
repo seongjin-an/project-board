0e14cb6d2836baf15db7579fa340dbf4b39e2e62
seongjin-an <seongjin4us@gmail.com>
2023-11-18 18:39:31 +0900

    #71 - 회원 정보 서비스 로직의 구현
    회원 정보를 다루는 서비스 로직이 기존에 없었는데,
    이번 기회에 간단한 형태로 구현함
    회원 정보의 조회와 저장을 담당

---
+++ src/main/java/com/ansj/demo/service/UserAccountService.java
@@ -0,0 +1,35 @@
+package com.ansj.demo.service;
+
+import com.ansj.demo.domain.UserAccount;
+import com.ansj.demo.dto.UserAccountDto;
+import com.ansj.demo.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class UserAccountService {
+
+    private final UserAccountRepository userAccountRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<UserAccountDto> searchUser(String username) {
+        return userAccountRepository.findById(username)
+                .map(UserAccountDto::from);
+    }
+
+    public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo) {
+        return UserAccountDto.from(
+                userAccountRepository.save(
+                        UserAccount.of(username, password, email, nickname, memo, username)
+                )
+        );
+    }
+
+}
